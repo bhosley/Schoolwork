dc5fd2f68c9ced6fbd27ea94f8ea42196bb4841d
bhosley <brandonscotthosley@gmail.com>
2020-09-12 13:12:54 -0600

    Update HW03RScript.R

--- 2020-Fall Advanced Statistical Methods/HW 03/HW03RScript.R
+++ 2020-Fall Advanced Statistical Methods/HW 03/HW03RScript.R
@@ -1,8 +1,28 @@
 # Load Boston data set
 library(MASS)
 
+mount(Boston)
+par(mfrow=c(2,7))
+plot(crim,  crim); abline(lm(crim~crim)) 
+plot(zn,    crim); abline(lm(zn~crim)) 
+plot(indus, crim); abline(lm(indus~crim)) 
+plot(chas,  crim); abline(lm(chas~crim))
+plot(nox,   crim); abline(lm(nox~crim)) 
+plot(rm,    crim); abline(lm(rm~crim))  
+plot(age,   crim); abline(lm(age~crim))  
+plot(dis,   crim); abline(lm(dis~crim))  
+plot(rad,   crim); abline(lm(rad~crim))  
+plot(tax,   crim); abline(lm(tax~crim))    
+plot(ptratio, crim); abline(lm(ptratio~crim))
+plot(black, crim); abline(lm(black~crim))
+plot(lstat, crim); abline(lm(lstat~crim))
+plot(medv,  crim); abline(lm(medv~crim))
 
-attach(Boston)
 
-lm.fit=lm(crim~medv)
-plot(crim~medv)
+
+
+library(reshape2)
+
+b <- melt(Boston, "crim")
+
+ggplot(b,aes(crim,value)) + facet_grid(.~variable) 
