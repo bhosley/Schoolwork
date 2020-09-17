9e789d3544ebe1120d3cc7bd6e8a08c90148bdee
bhosley <brandonscotthosley@gmail.com>
2020-09-17 05:33:39 -0600

    Update lab2a.sh

--- 2020-Fall NoSQL Databases/lab2a.sh
+++ 2020-Fall NoSQL Databases/lab2a.sh
@@ -8,6 +8,12 @@
 # key - NameOfMovieInPascalCase
 
 # 1. Add six movies from at least 2 genres
+curl -v -X PUT http://riak:8098/riak/movies/OceansEleven -H "Content-Type: application/json" -d '{"genre" : "thriller", "releasedate" : "2001", "runningtime" : "1:56"}'
+curl -v -X PUT http://riak:8098/riak/movies/TheDarkKnight -H "Content-Type: application/json" -d '{"genre" : "action", "releasedate" : "2008", "runningtime" : "2:32"}'
+curl -v -X PUT http://riak:8098/riak/movies/SavingPrivateRyan -H "Content-Type: application/json" -d '{"genre" : "war", "releasedate" : "1998", "runningtime" : "2:49"}'
+curl -v -X PUT http://riak:8098/riak/movies/HacksawRidge -H "Content-Type: application/json" -d '{"genre" : "war", "releasedate" : "2016", "runningtime" : "2:26"}'
+curl -v -X PUT http://riak:8098/riak/movies/Inception -H "Content-Type: application/json" -d '{"genre" : "thriller", "releasedate" : "2010", "runningtime" : "2:28"}'
+curl -v -X PUT http://riak:8098/riak/movies/Primer -H "Content-Type: application/json" -d '{"genre" : "thriller", "releasedate" : "2004", "runningtime" : "1:17"}'
 
 # 2. Delete one of the movie records
 
