53f1467b2e12dd7eb8e79e9671ebc5bb8d1b2dfe
bhosley <brandonscotthosley@gmail.com>
2020-10-01 09:34:47 -0600

    Update lab3a.js

--- 2020-Fall NoSQL Databases/lab3a.js
+++ 2020-Fall NoSQL Databases/lab3a.js
@@ -35,66 +35,71 @@ db.users.find({"_id":ObjectId("5bb26043708926e438db6cad")})
 //      title, body, slug, author, 
 //      comments (array with objects containing user_id, comment, approved, created_at), 
 //      and category (array with objects containing name)
-db.blogs.insert({
-    title:"",
-    body:"",
-    slug:"",
-    author:"",
-    comments:[
-        {
-            user_id:ObjectId(""),
-            comment:"",
-            approved:true,
-            created_at:ISODate()
-        },
-    ],
-    category:[
-        {name:""}
-        {name:""}
-    ]
-})
 // The user_id and author fields should be one of the 3 users _id found above
 db.blogs.insert({
     title:"",
     body:"",
     slug:"",
-    author:"",
+    author:ObjectId("5bb26043708926e438db6cad"),
     comments:[
         {
-            user_id:ObjectId(""),
+            user_id:ObjectId("5bb26043708926e438db6caf"),
             comment:"",
             approved:true,
-            created_at:ISODate()
-        },
+            created_at:ISODate("2020-09-10")
+        }
     ],
     category:[
+        {name:""},
         {name:""}
+    ]
+})
+db.blogs.insert({
+    title:"",
+    body:"",
+    slug:"",
+    author:ObjectId("5bb26043708926e438db6cae"),
+    comments:[
+        {
+            user_id:ObjectId("5bb26043708926e438db6cad"),
+            comment:"",
+            approved:true,
+            created_at:ISODate("2020-09-21")
+        }
+    ],
+    category:[
+        {name:""},
         {name:""}
     ]
 })
 // One of the posts should contain the word "framework" in the body
 db.blogs.insert({
-    title:"",
+    title:"Thoughts on HTML as a Programming Language",
     body:"",
     slug:"",
-    author:"",
+    author:ObjectId("5bb26043708926e438db6caf"),
     comments:[
         {
-            user_id:ObjectId(""),
+            user_id:ObjectId("5bb26043708926e438db6cae"),
             comment:"",
             approved:true,
-            created_at:ISODate()
-        },
+            created_at:ISODate("2020-10-01")
+        },{
+            user_id:ObjectId("5bb26043708926e438db6caf"),
+            comment:"",
+            approved:false,
+            created_at:ISODate("2020-09-10")
+        }
     ],
     category:[
-        {name:""}
+        {name:""},
         {name:""}
     ]
 })
 // 3.1 Get all comments by User 5bb26043708926e438db6caf across all posts displaying only the title and slug
-db.blogs.find({body:/Framwork/i},
-    {title:1,body:1,slug:0,author:0,comments:0,category:0})
-    
+db.blogs.find({'comments.user_id':ObjectId("5bb26043708926e438db6caf")},
+    {title:1,body:0,slug:1,author:0,comments:0,category:0})
+
 // 4. Select a blog via a case-insensitive regular expression containing the word Framework in the body displaying only the title and body
 db.blogs.find({body:/Framwork/i},
     {title:1,body:1,slug:0,author:0,comments:0,category:0})
