464d8b9c9a09967a4a487e623848dec5e3f2e227
bhosley <brandonscotthosley@gmail.com>
2020-11-03 10:39:23 -0700

    Update HW6RScript.R

--- 2020-Fall Advanced Statistical Methods/HW 06/HW6RScript.R
+++ 2020-Fall Advanced Statistical Methods/HW 06/HW6RScript.R
@@ -24,7 +24,7 @@ points (3,reg.summary$bic [3],col="red",cex=2,pch =20)
 set.seed(1)
 train=sample(c(TRUE ,FALSE), nrow(Boston),rep=TRUE)
 test=(!train)
-regfit.best=regsubsets (crim~.,data=Boston[train ,], nvmax=13)
+regfit.best=regsubsets(crim~.,data=Boston[train ,], nvmax=13)
 test.mat=model.matrix(crim~.,data=Boston[test ,])
 val.errors =rep(NA ,13)
 for(i in 1:13){coefi=coef(regfit.best ,id=i)
@@ -32,14 +32,21 @@ for(i in 1:13){coefi=coef(regfit.best ,id=i)
   val.errors[i]=mean((Boston$crim[test]-pred)^2)
 }
 ## Cross-Validation
+predict.regsubsets = function(object , newdata ,id ,...){
+  form=as.formula (object$call [[2]])
+  mat=model.matrix(form ,newdata )
+  coefi=coef(object ,id=id)
+  xvars=names(coefi)
+  mat[,xvars]%*%coefi
+}
 k <- 10
 folds=sample (1:k,nrow(Boston),replace=TRUE)
 cv.errors=matrix (NA,k,13, dimnames=list(NULL, paste (1:13)))
 for(j in 1:k){
-  best.fit=regsubsets(crim~.,data=Boston [folds!=j,],nvmax=13)
+  best.fit <- regsubsets(crim~., data=Boston[folds!=j,],nvmax=13)
   for(i in 1:13){
-    pred=predict(best.fit ,Boston [folds ==j,],id=i)
-    cv.errors[j,i]= mean(( Boston$crim[folds==j]-pred)^2)
+    pred <- predict(best.fit ,Boston[folds ==j,], id=i)
+    cv.errors[j,i] <- mean(( Boston$crim[folds==j]-pred)^2)
   }
 }
 mean.cv.errors=apply(cv.errors ,2, mean)
@@ -47,15 +54,46 @@ mean.cv.errors
 par(mfrow=c(1,1))
 plot(mean.cv.errors ,type='b')
 reg.best=regsubsets (crim~.,data=Boston , nvmax=13)
-coef(reg.best ,11)
+coef(reg.best ,12)
 
+# 1.2 Ridge Regression
+library(glmnet)
 
+x=model.matrix(crim~.,Boston )[,-1]
+y=Boston$crim
 
-# 1.2 The Lasso
+grid=10^seq(10,-2, length =100)
+ridge.mod=glmnet (x,y,alpha=0, lambda=grid)
+dim(coef(ridge.mod))
 
-# 1.3 Ridge Regression
+train=sample (1: nrow(x), nrow(x)/2)
+test=(-train)
+y.test=y[test]
+## Cross-validation
+cv.out=cv.glmnet(x[train ,],y[ train],alpha=0)
+plot(cv.out)
+bestlam =cv.out$lambda.min
+bestlam
+ridge.pred=predict (ridge.mod ,s=bestlam ,newx=x[test ,])
+mean((ridge.pred -y.test)^2)
+
+# 1.3 The Lasso
+lasso.mod=glmnet(x[train ,],y[ train],alpha=1, lambda =grid)
+plot(lasso.mod)
+cv.out=cv.glmnet(x[train ,],y[ train],alpha=1)
+
+plot(cv.out)
+bestlam =cv.out$lambda.min
+lasso.pred=predict (lasso.mod ,s=bestlam ,newx=x[test ,])
+mean((lasso.pred -y.test)^2)
+
+out=glmnet (x,y,alpha=1, lambda=grid)
+lasso.coef=predict (out ,type="coefficients",s= bestlam) [1:14,]
+lasso.coef
 
 # 1.4 PCR
+library(pls)
+
 
 # 2.1 Validation Set Error
 
