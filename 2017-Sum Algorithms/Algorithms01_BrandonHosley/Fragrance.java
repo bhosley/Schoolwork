5fbeb398f8ead1745c433cb846a4341eb94bf2ea
bhosley <brandonscotthosley@gmail.com>
2020-12-12 06:32:07 -0700

    Create Fragrance.java

---
+++ 2017-Sum Algorithms/Algorithms01_BrandonHosley/Fragrance.java
@@ -0,0 +1,18 @@
+
+public class Fragrance {
+	String smell;
+	int cost;
+	
+	public Fragrance(String iSmell, int iCost) {
+		smell = iSmell;
+		cost = iCost;
+	}
+	
+	public String smell() {
+		return smell;
+	}
+	
+	public int cost() {
+		return cost;
+	}
+}
