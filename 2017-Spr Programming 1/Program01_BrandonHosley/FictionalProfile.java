acf559a4e081a7c85057a4e8040a5d5da454f188
bhosley <brandonscotthosley@gmail.com>
2020-12-13 03:44:59 -0700

    Create FictionalProfile.java

---
+++ 2017-Spr Programming 1/Program01_BrandonHosley/FictionalProfile.java
@@ -0,0 +1,13 @@
+// Brandon Hosley
+// CSC320 - Programming 1
+// 2018 01 15
+
+public class FictionalProfile {
+	public static void main(String[] args) {
+		System.out.println("John");
+		System.out.println("Doe");
+		System.out.println("1600 Pennsylvania Ave.");
+		System.out.println("Washington D.C.");
+		System.out.println("20500");
+	}
+}
