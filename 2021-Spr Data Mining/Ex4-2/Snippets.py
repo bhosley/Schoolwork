ac87e5e841db420f2c61dac143d40c0e50b6a736
bhosley <brandonscotthosley@gmail.com>
2021-03-29 13:26:08 -0600

    Update Snippets.py

--- 2021-Spr Data Mining/Ex4-2/Snippets.py
+++ 2021-Spr Data Mining/Ex4-2/Snippets.py
@@ -7,6 +7,7 @@ raw_df.describe().show()
 
 from pyspark.ml.feature import Imputer
 imputer = Imputer(strategy='mean', inputCols=['Age'], outputCols=['ImputedAge'])
+filtered_df = raw_df.select(['Survived', 'Pclass', 'Gender', 'Age', 'SibSp', 'Parch', 'Fare'])
 imputed_df = imputer.fit(filtered_df).transform(filtered_df)
 imputed_df.show()
 
@@ -25,19 +26,20 @@ features_df = assembler.transform(indexed_df)
 # Splitting Dataset
 
 (trainingData, testData) = features_df.randomSplit([0.8, 0.2])
-features_df.count()
 trainingData.count()
 testData.count()
+trainingData.count() + testData.count()
+features_df.count()
 
 # trainingData
 
-from org.apache.spark.ml.classification import RandomForestClassifier
+from apache.spark.ml.classification import RandomForestClassifier
 rf = RandomForestClassifier(labelCol="Survived", featuresCol="features")
 modelRF = rf.fit(trainingData)
 
 # Evaluate
 
-from org.apache.spark.ml.evaluation import BinaryClassificationEvaluator
+from apache.spark.ml.evaluation import BinaryClassificationEvaluator
 
 predictions = modelRF.transform(testData)
 evaluator = BinaryClassificationEvaluator(labelCol="Survived", predictionCol="predictions", metricName="Accuracy")
