651b7cff1036370915c04dcf1520ca5ce842f098
bhosley <brandonscotthosley@gmail.com>
2021-04-24 02:56:45 -0600

    Update Snippets.py

--- 2021-Spr Data Mining/Ex6-1/Snippets.py
+++ 2021-Spr Data Mining/Ex6-1/Snippets.py
@@ -1,85 +1,62 @@
-hadoop fs -put /home/data/CSC533DM/uber.csv 
-hadoop fs -ls /user/data/CSC533DM/
-head /user/data/CSC533DM/uber.csv 
-
 $ pyspark
 
-## 1.2.1
+# raw_df = spark.read.option("header", "true").option("inferSchema", "true").csv("/user/data/CSC533DM/titanic.csv")
+raw_df = spark.read.csv("/user/data/CSC533DM/titanic.csv", header=True, inferSchema=True)
+raw_df.describe().show()
 
-schema = '`Date/Time` TIMESTAMP, Lat DOUBLE, Lon DOUBLE, Base STRING'
-uber_df = spark.read.schema(schema) \
-    .option("header","true") \
-    .option("timestampFormat", "M/d/yyyy HH:mm:ss") \
-    .csv("/user/data/CSC533DM/uber.csv")
-uber_df.printSchema()
-uber_df.show(5)
-
-##
-
-# Import the class
-from pyspark.ml.feature import VectorAssembler 
-# Creating a vector
-assembler = VectorAssembler(inputCols=['Lat', 'Lon'], outputCol='features') 
-# Transform
-features_df = assembler.transform(uber_df)
-features_df.show(5)
-
-## 2.1.1
-from pyspark.ml.clustering import KMeans
-from pyspark.ml.evaluation import ClusteringEvaluator
-
-kmeans3 = KMeans().setK(3).setSeed(1)
-kmeans5 = KMeans().setK(5).setSeed(1)
-kmeans8 = KMeans().setK(8).setSeed(1)
-
-model3 = kmeans3.fit(features_df)
-model5 = kmeans5.fit(features_df)
-model8 = kmeans8.fit(features_df)
-
-predictions3 = model3.transform(features_df)
-predictions5 = model5.transform(features_df)
-predictions8 = model8.transform(features_df)
-
-evaluator = ClusteringEvaluator()
-
-silhouette3 = evaluator.evaluate(predictions3)
-silhouette5 = evaluator.evaluate(predictions5)
-silhouette8 = evaluator.evaluate(predictions8)
-print("Silhouette with squared euclidean distance = " + str(silhouette3))
-print("Silhouette with squared euclidean distance = " + str(silhouette5))
-print("Silhouette with squared euclidean distance = " + str(silhouette8))
-
-centers = model3.clusterCenters()
-print("Cluster Centers: ")
-for center in centers:
-    print(center)
-
-centers = model5.clusterCenters()
-print("Cluster Centers: ")
-for center in centers:
-    print(center)
-
-centers = model8.clusterCenters()
-print("Cluster Centers: ")
-for center in centers:
-    print(center)
-
-## 3.1.1
-
-## 4
-
-from pyspark.sql.functions import hour, desc
-predictions8.groupBy(hour("Date/Time").alias("hour")) \
-    .count().sort(desc("count")).show(24)
+filtered_df = raw_df.select(['Survived', 'Pclass', 'Gender', 'Age', 'SibSp', 'Parch', 'Fare'])
 
 
-(uber_df
-    .groupBy(hour("Date/Time").alias("hour"))
-    .agg(count("value").alias("pickups"))
-    .sort("pickups")
-    .show())
+filtered_df.select('Fare').describe().show()
+
+## Fare
+quantiles = filtered_df.approxQuantile("Fare", [0.25, 0.75], 0.0)
+Q1, Q3 = quantiles[0], quantiles[1]
+IQR = Q3-Q1
+fareLowerRange, fareUpperRange = Q1 - 1.5 * IQR, Q3 + 1.5 * IQR
+fareLowerRange, fareUpperRange
+
+trimmed_df = filtered_df \
+    .filter(filtered_df.Fare > fareLowerRange) \
+    .filter(filtered_df.Fare < fareUpperRange)
+trimmed_df.count()
 
 
-## 5
+## Age
 
-predictions8.groupBy("prediction").count().show(8)
+# Check for Nulls
+filtered_df.filter(filtered_df.Age.isNull()).count()
+# Impute missing values with arithmetic mean
+from pyspark.ml.feature import Imputer
+imputer = Imputer(strategy='mean', inputCols=['Age'], outputCols=['ImputedAge'])
+filtered_df = imputer.fit(filtered_df).transform(filtered_df)
+
+# Find IQR 
+quantiles = filtered_df.approxQuantile('ImputedAge', [0.25, 0.75], 0.0)
+Q1, Q3 = quantiles[0], quantiles[1]
+IQR = Q3-Q1
+ageLowerRange, ageUpperRange = Q1 - 1.5 * IQR, Q3 + 1.5 * IQR
+# Count outliers
+filtered_df.filter(filtered_df.ImputedAge < ageLowerRange).count() + \
+filtered_df.filter(filtered_df.ImputedAge > ageUpperRange).count()
+
+# Re-trim
+trimmed_df = filtered_df \
+    .filter(filtered_df.Fare > fareLowerRange) \
+    .filter(filtered_df.Fare < fareUpperRange) \
+    .filter(filtered_df.ImputedAge > ageLowerRange) \
+    .filter(filtered_df.ImputedAge < ageUpperRange)
+trimmed_df.count()
+
+
+## Parch
+
+# Check for Nulls
+filtered_df.filter(filtered_df.Parch.isNull()).count()
+
+
+
+## SibSp
+
+# Check for Nulls
+filtered_df.filter(filtered_df.SibSp.isNull()).count()
