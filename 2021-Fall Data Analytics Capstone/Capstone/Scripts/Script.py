c945558ac4ef6757a017ae2addc78c4fb6ad1aa5
bhosley <brandonscotthosley@gmail.com>
2021-11-18 21:21:15 +0100

    Update Script.py

--- 2021-Fall Data Analytics Capstone/Capstone/Scripts/Script.py
+++ 2021-Fall Data Analytics Capstone/Capstone/Scripts/Script.py
@@ -8,21 +8,24 @@ FIDUCIAL_THRESHOLD = 3000
 MARKER_THRESHOLD = 500
 MARKER_RANGE = np.array([[0, 0, 255],[255, 255, 255]])
 
-# 
+#################
+#   Functions   #
+#################
+
 def find_origin(frame):
     x,y,radius = 0,0,0
     for _ in range(100):     
         mask = cv2.inRange(frame, 
             np.array([0, 0, 0]),  # BGR Black
             np.array([75, 75, 75]))  # BGR lighter Black
-
         contours,h = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        # Checking if any contour is detected then run the following statements
+
         if len(contours) > 0:
-            cmax = max(contours, key = cv2.contourArea) # Largest Contour
-            area = cv2.contourArea(cmax)
+            largest_contour = max(contours, key = cv2.contourArea) # Largest Contour
+            area = cv2.contourArea(largest_contour)
+            
             if area > FIDUCIAL_THRESHOLD:
-                (cx,cy),r = cv2.minEnclosingCircle(cmax)
+                (cx,cy),r = cv2.minEnclosingCircle(largest_contour)
                 x += cx
                 y += cy
                 radius += r
@@ -31,37 +34,39 @@ def find_origin(frame):
     radius = radius/100
     return center, radius
 
-# Program Loop
+#####################
+#   Program Init    #
+#####################
+
 ret, frame = CAP.read()
 origin, radius = find_origin(frame)
 mx, my = 0,0
+
+#####################
+#   Program Loop    #
+#####################
+
 while True:
-    ret, frame = CAP.read()
     # Exit condition
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    # Identify Marker
+    # Video capture and identify contours of potential markers
+    ret, frame = CAP.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
     marker_mask = cv2.inRange(hsv, MARKER_RANGE[0], MARKER_RANGE[1])
-    marker_mask = cv2.dilate(marker_mask, KERNEL, iterations=1)
-    
+    marker_mask = cv2.dilate(marker_mask, KERNEL, iterations=2)
     contours,h = cv2.findContours(marker_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # Checking if any contour is detected then run the following statements
-    if len(contours) > 0:        
-        # Get the biggest contour from all the detected contours
-        cmax = max(contours, key = cv2.contourArea)
-        # Find the area of the contour
-        area = cv2.contourArea(cmax)
-        # Checking if the area of the contour is greater than a threshold
+    
+    if len(contours) > 0:
+        largest_contour = max(contours, key = cv2.contourArea)
+        area = cv2.contourArea(largest_contour)
+
+        # If large enough, return coordinates of contour's moment 
         if area > MARKER_THRESHOLD:
-            # Find center point of the contour
-            M = cv2.moments(cmax)
+            M = cv2.moments(largest_contour)
             mx = int(M["m10"] / M["m00"])
             my = int(M["m01"] / M["m00"])
-    else:
-        previous_center_point= 0
     
     # Export Data
 
@@ -71,9 +76,11 @@ while True:
     cv2.circle(frame, (int(origin[0]),int(origin[1])), int(radius), (255, 0, 0), 2)
     cv2.circle(frame, (int(mx),int(my)), 1, (0, 0, 255), 2)
     cv2.imshow('Detected Circles', frame)
-    cv2.imshow('Mask', marker_mask)
+    #cv2.imshow('Mask', marker_mask)
 
-# End While-Loop
+####################
+#   Program End    #
+####################
 
 CAP.release()
 cv2.destroyAllWindows()
