f93bab7dd5cebf7ffd3e834f7924b5ce970179d5
bhosley <brandonscotthosley@gmail.com>
2023-02-27 21:29:15 -0500

    Update ProjectFunctions.py

--- 2023Q1 - Stochastic Modeling and Analysis/Project/ProjectFunctions.py
+++ 2023Q1 - Stochastic Modeling and Analysis/Project/ProjectFunctions.py
@@ -22,8 +22,7 @@ def create_one_step_P_matrix(
     # Construct State space S, as an 1820x5 array; 
     # row as an index label for the 5-tuple state variable
     S = []
-    iterable_ACFT = range(NUM_ACFT+1)
-    for x0 in iterable_ACFT:
+    for x0 in range(NUM_ACFT+1):
         for x1 in range(NUM_ACFT+1-x0):
             for x2 in range(NUM_ACFT+1-x0-x1):
                 for x3 in range(NUM_ACFT+1-x0-x1-x2):
@@ -32,13 +31,12 @@ def create_one_step_P_matrix(
     # Convert list to numpy array
     S= np.array(S)
     cardS = len(S)
-    iterable_cardS = range(cardS)
 
     ## Compute 1-step transition probability matrix
     # with timer
     tic = time.perf_counter()
     P = np.matrix(np.zeros((cardS,cardS)))
-    for i_index in iterable_cardS:
+    for i_index in range(cardS):
         # Obtain State Variable
         X_n = index_to_state(S,i_index)
 
@@ -47,15 +45,15 @@ def create_one_step_P_matrix(
         # pmf of number of acft inbound to front shop from MC post inspection
         num_MC = X_n[0]
         num_acft_fly = np.min((MAX_SORTIES, num_MC))
-        pmf_Yn_MC_failed = [binom.pmf(y,num_acft_fly,1-PROB_PASS_POSTFLT_INSP) for y in iterable_ACFT]
+        pmf_Yn_MC_failed = [binom.pmf(y,num_acft_fly,1-PROB_PASS_POSTFLT_INSP) for y in range(NUM_ACFT+1)]
         # PMF of acft inbound to front shop from back shop inspection
         num_BS3 = X_n[4]
-        pmf_Yn_BS3_failed = [binom.pmf(y,num_BS3,1-PROB_PASS_POST_BACKSHOP_INSP) for y in iterable_ACFT]
+        pmf_Yn_BS3_failed = [binom.pmf(y,num_BS3,1-PROB_PASS_POST_BACKSHOP_INSP) for y in range(NUM_ACFT+1)]
         pmf_Xnpl_FS = np.convolve(pmf_Yn_MC_failed,pmf_Yn_BS3_failed)
         
         ### Compute PMF of X_npl[2], Back Shop 1
         num_FS = X_n[1]
-        pmf_Xnpl_BS1 = [binom.pmf(y,num_FS,1-PROB_REPAIR_FRONTSHOP) for y in iterable_ACFT]
+        pmf_Xnpl_BS1 = [binom.pmf(y,num_FS,1-PROB_REPAIR_FRONTSHOP) for y in range(NUM_ACFT+1)]
         ### Compute PMF of X_npl[3], Back Chop 2
         pmf_Xnpl_BS2 = np.zeros((NUM_ACFT+1))
         pmf_Xnpl_BS2[X_n[2]] = 1
@@ -67,12 +65,12 @@ def create_one_step_P_matrix(
 
         ### Assign 1-step probabilities based on PMFs
         ## SLOWER METHOD:
-        # for j_index in iterable_cardS:
+        # for j_index in range(cardS):
         #     X_npl = index_to_state(j_index)
         #     P[i_index,j_index] = \
         #         pmf_Xnpl_FS[X_npl[1]]*pmf_Xnpl_BS1[X_npl[2]]*pmf_Xnpl_BS2[X_npl[3]]*pmf_Xnpl_BS3[X_npl[4]]
         ## FASTER METHOD:
-        for j_index in (j_index_filtered for j_index_filtered in iterable_cardS if \
+        for j_index in (j_index_filtered for j_index_filtered in range(cardS) if \
                         index_to_state(S,j_index_filtered)[4]==X_n[3] and
                         index_to_state(S,j_index_filtered)[3]==X_n[2] and
                         index_to_state(S,j_index_filtered)[2]<=X_n[1] and
