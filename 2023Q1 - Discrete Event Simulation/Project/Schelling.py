a7fd9865c65b06eb5fd18ac68ba0592dd40873e8
bhosley <brandonscotthosley@gmail.com>
2023-03-07 21:07:12 -0500

    Update Schelling.py

--- 2023Q1 - Discrete Event Simulation/Project/Schelling.py
+++ 2023Q1 - Discrete Event Simulation/Project/Schelling.py
@@ -1,4 +1,6 @@
 import numpy as np 
+from math import floor
+from collections import Counter
 import matplotlib.pyplot as plt  
 import matplotlib.animation as animation 
 # Uncomment to display in .ipynb
@@ -6,9 +8,9 @@ from IPython.display import HTML
 
 
 class Agent:
-    def __init__(self, membership, numTeams, h=0.3, i=0, j=0):
+    def __init__(self, membership, numTeams, ufunc, i=0, j=0):
        self.team = membership
-       self.homophily = h
+       self.ufunc = ufunc
        self.unhappy = False
        self.color = membership/numTeams
        self.i = i
@@ -19,7 +21,7 @@ def initAgents(a,demographic):
     numTeams = len(demographic)
     for key,value in demographic.items():
         for _ in range(value['population']):
-            a.append(Agent(key,numTeams,h=value['tolerance']))
+            a.append(Agent(key,numTeams,value['ufunc']))
     return a
 
 
@@ -38,34 +40,31 @@ def placeAgents(agents,grid,gridSize,):
     return [agents,grid]
 
 
-def checkHappy(agents,grid):
+def checkHappy(agents,grid,nbrhd=3):
     uha = []
     numberUnhappy = 0
+    dim = floor(nbrhd/2)
     for agent in agents:
-        nSame = 0
-        nDifferent = 0
+        neighbors = []
         i = agent.i
         j = agent.j
-        for m in range(int(i-1),int(i+2)):
-            for n in range(int(j-1),int(j+2)):
-                if (grid[m,n]==agent.color):
-                    nSame=nSame+1
-                elif(grid[m,n]!=0.0):
-                    nDifferent = nDifferent+1
+        for m in range(int(i-dim),int(i+dim+1)):
+            for n in range(int(j-dim),int(j+dim+1)):
+                neighbors.append(grid[m,n])
+        
+        tally = Counter(neighbors)
+        like = tally[agent.color]
         # correct for self counting
-        nSame = nSame - 1
-        nBoth = nSame + nDifferent
-        if(nBoth == 0):
+        like = like-1
+        if(agent.ufunc(like)):
             agent.unhappy = False
-        elif(agent.homophily >=(((nSame)/(nBoth)))):
+        else:
             agent.unhappy = True
             numberUnhappy = numberUnhappy + 1
-        else:
-            agent.unhappy = False
     return uha, numberUnhappy
 
 
-def update(frameNum, img, grid, gridSize, agents,x,y,line):  
+def update(frameNum,grid,gridSize,agents,x,y,nbrhd=3,img=None,line=None):  
     # copy grid since we require 8 neighbors  
     # for calculation and we go line by line  
     newGrid = np.zeros([gridSize+2,gridSize+2]) 
@@ -84,21 +83,50 @@ def update(frameNum, img, grid, gridSize, agents,x,y,line):
         newGrid[agent.i,agent.j] = agent.color
         
     # check for unhappy agents
-    _,n = checkHappy(agents,newGrid)
+    _,n = checkHappy(agents,newGrid,nbrhd)
     # update data 
-    img.set_data(newGrid) 
+    if img: img.set_data(newGrid) 
     grid[:] = newGrid[:] 
 
     x.append(frameNum)
     y.append(n)
-    line.set_data(x,y)
-    return img, line
+    if line: line.set_data(x,y)
+    return img, x, y, line
 
 
-def run_experiment(demographic=None,gridSize=100,withAnimation=False):
+#run trials without animations
+def run_experiment(demographic=None,gridSize=100,nbrhd=3,frames=50,reps=10,filename='test'):
     demographics = demographic or {
         1:{'population':5000,'tolerance':0.3},
         2:{'population':1000,'tolerance':0.3}}
+    big_Y = np.array(range(frames), ndmin=2)
+    for r in range(reps):
+        #initialize grid
+        grid = np.zeros([gridSize+2,gridSize+2])
+
+        #instantiate agents
+        agents = []
+        agents = initAgents(agents,demographics)
+
+        #place agents
+        [agents,grid]= placeAgents(agents,grid,gridSize)
+        #data collection
+        x,y =[],[]
+        for f in range(frames):
+            _,x,y,_ = update(f,grid, gridSize, agents,x,y,nbrhd)
+        big_Y = np.concatenate((big_Y,np.array(y,ndmin=2)),axis=0)
+    np.savetxt("outputs/{}.csv".format(filename), 
+           big_Y.T,
+           delimiter =", ", 
+           fmt ='% s')
+    
+
+#run single trial with animation
+def run_animation(demographic=None,gridSize=100,frames=50,nbrhd=3):
+    demographics = demographic or {
+        1:{'population':5000,'tolerance':0.3},
+        2:{'population':1000,'tolerance':0.3}}
+
     #initialize grid
     grid = np.zeros([gridSize+2,gridSize+2])
 
@@ -109,40 +137,25 @@ def run_experiment(demographic=None,gridSize=100,withAnimation=False):
     #place agents
     [agents,grid]= placeAgents(agents,grid,gridSize)
     #data collection
-    move,unhappiness =[],[]
-    #run trials without animations
-    if not withAnimation:
-        _ =+ 1
-    #run trials with animation
-    else:   
-        # set up animation 
-        updateInterval = 50
-        fig, (ax1,ax2) = plt.subplots(1,2) 
-        img = ax1.imshow(grid, interpolation='nearest') 
-        line, = ax2.plot([], [], lw=3)
-        ax2.set(xlim=(0,50), ylim=(0,200), aspect='0.25')
-        ax1.set_title('Field')
-        ax2.set_title('Total Unhappiness')
-        ani = animation.FuncAnimation(fig, update, fargs=(img, grid,gridSize, agents,move,unhappiness,line, ), 
-                                    frames = 50, 
-                                    interval=updateInterval, 
-                                    save_count=50) 
+    x,y =[],[]
+    # set up animation 
+    fig, (ax1,ax2) = plt.subplots(1,2) 
+    img = ax1.imshow(grid, interpolation='nearest') 
+    line, = ax2.plot([], [], lw=3)
+    height = sum([demo['population'] for demo in demographics.values()])
+    width = frames
+    ax2.set(xlim=(0,width), ylim=(0,height), aspect=width/height)
+    #ax2.set_yscale('log')
+    ax1.set_title('Field')
+    ax2.set_title('Total Unhappiness')
+    ani = animation.FuncAnimation(fig,update,
+                                fargs=(grid,gridSize,agents,x,y,nbrhd,img,line,), 
+                                frames = frames, 
+                                interval=100, 
+                                repeat_delay = 9000,
+                                save_count=50) 
+
+    #plt.show() 
     
-        #plt.show() 
-
-        # uncomment to display in .ipynb
-        return HTML(ani.to_jshtml())
-
-# demographic -> label: [population,tolerance]
-demographics = {
-    1:{
-    'population':4000,
-    'tolerance':0.3},
-    2:{
-    'population':1000,
-    'tolerance':0.3},
-    3:{
-    'population':1000,
-    'tolerance':0.3}
-}
-run_experiment(demographics,withAnimation=True)
+    # uncomment to display in .ipynb
+    return HTML(ani.to_jshtml())
