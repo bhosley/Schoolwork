19ffdad6c7a8b06c5b858fc4565c5602ffea693e
bhosley <brandonscotthosley@gmail.com>
2023-08-23 00:04:51 -0400

    Create OPER 618 - Lsn 16 Coalition Game Analysis - Convex - Primal BS.py

---
+++ 2023Q3 - Game Theory/OPER 618 - Lsn 16 Coalition Game Analysis - Convex - Primal BS.py
@@ -0,0 +1,38 @@
+# Coded by Dr. Brian J. Lunday, AFIT/ENS on 20 April 2022
+# In support of OPER 618 Game Theory & Math Programming, Summer 2022
+
+import pyomo.environ as pyo
+
+m = pyo.ConcreteModel()
+
+# Input sets
+m.I = {1,2,3} # pllayers
+m.S = {1,2,3,4,5,6,7,8}
+m.subsetS = {2,3,4,5,6,7,8}
+
+# Dictionary of subset membership
+subset =[[0,0,0,0,0,0,0,0,0],
+         [0,0,1,0,0,1,1,0,1],
+         [0,0,0,1,0,1,0,1,1],
+         [0,0,0,0,1,0,1,1,1],]
+
+# Define parameters
+m.nu = {1:0,2:0.1,3:0.2,4:0.15,5:0.5,6:0.25,7:0.75,8:1}
+
+# Define decision variables
+m.x = pyo.Var(m.I,domain=pyo.NonNegativeReals)
+
+# Formulate model
+m.objfnvalue = pyo.Objective(expr = sum(m.x[i] for i in m.I),sense = pyo.minimize)
+m.StableCoalitionConstraint=pyo.ConstraintList()
+for s in m.subsetS:
+    m.StableCoalitionConstraint.add(sum(subset[i][s]*m.x[i] for i in m.I) >= m.nu[s])
+
+# Solve model
+results=pyo.SolverFactory('cbc').solve(m,tee=False)
+
+# Output results
+print('z = ',pyo.value(m.objfnvalue))   
+for i in m.I:
+    print('x(',i,') = ', pyo.value(m.x[i]))
+        
