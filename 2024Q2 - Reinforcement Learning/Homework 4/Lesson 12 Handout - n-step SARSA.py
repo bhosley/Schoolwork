594648df302c72f0aa179c0e7bf77aefdb92d21e
bhosley <brandonscotthosley@gmail.com>
2024-05-20 21:20:17 -0400

    HW 4 updates

--- 2024Q2 - Reinforcement Learning/Homework 4/Lesson 12 Handout - n-step SARSA.py
+++ 2024Q2 - Reinforcement Learning/Homework 4/Lesson 12 Handout - n-step SARSA.py
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 import gymnasium as gym
 import numpy as np
 import time
@@ -7,6 +15,7 @@ from sklearn import metrics
 import collections
 
 env = gym.make('MountainCar-v0')
+
 print(f"\nFarama OpenAI Gym version: {gym.__version__}")
 
 # Observation and Action Space
@@ -18,33 +27,53 @@ print("The action space: {}".format(env.action_space))
 # Learning Rate
 alpha_a = 1.0
 alpha_b = 0.5
-
 def alpha(n):
     return alpha_a/(1+n)**alpha_b
 
 # Eps-Greedy stepsize rule
 eps_a = 1.0
 eps_b = 0.5
-
 def epsilon(n):
     return eps_a/(1+n)**eps_b
 
-qinit = 0.5               # Q initialization
-nm1 = 8                   # Number of steps for computing n-step returns; nm1 = 0 indicat4es 1-step return
-Sintervals = 11           # Number of intervals per dimension
+# Q initialization
+qinit = 0.5
+
+# Number of steps for computing n-step returns; nm1 = 0 indicat4es 1-step return
+nm1 = 1
+
+# Number of intervals per dimension
+# e.g., using 11 intervales indicates 12 discrete states per dim
+Sintervals = 11
 
 ## Algorithm Parameters
-gamma = 0.999             # Discount rate 
-qrange = [-200,-100]      # Range of q-values for invironment
-Z = 10                    # Number of algorithm replications
-M = int(0.5e3)            # Number of Episodes
-test_freq = 25            # Policy Evaluation test frequency
-num_test_reps = 30        # Number of replications per test
-offset = 0                # Seed offset for manual testing
 
-# State/Action Space
-num_state_dims = len(env.observation_space.low) # Continuous state space
-num_actions = env.action_space.n                # Discrete Action Space
+# Discount rate 
+gamma = 0.999
+
+# Range of q-values for environment
+qrange = [-200,-100]
+
+# Number of algorithm replications
+Z = 4 #Z = 10
+
+# Number of Episodes
+M = int(0.5e3) #M = int(0.5e3)
+
+# Policy Evaluation test frequency
+test_freq = 25
+
+# Number of replications per test
+num_test_reps = 30
+
+# Seed offset for manual testing
+offset = 0 
+
+# Continuous state space
+num_state_dims = len(env.observation_space.low) 
+
+# Discrete Action Space
+num_actions = env.action_space.n                
 
 # Define reference points for dicretizing state space
 Slow = np.array(env.observation_space.low)
@@ -52,13 +81,13 @@ Shigh = np.array(env.observation_space.high)
 Srange = Shigh - Slow
 Sunit = Srange/Sintervals
 Ssize = (Sintervals + 1)*np.ones(num_state_dims).astype(int)
-Smax = Srange/Sunit
-SAsize = np.append(Ssize,num_actions)     # Size of State Action Space
+
+# Size of State Action Space
+SAsize = np.append(Ssize,num_actions)
 
 # Convert continuous state to discrete representation
-def phi(Scont) :  
-    temp = np.round((Scont-Slow)/Sunit).astype(int)
-    return np.minimum(temp, Smax).astype(int)
+def phi(Scont):  
+    return np.round((Scont-Slow)/Sunit).astype(int) # Smax = Srange/Sunit # temp =  #return np.minimum(temp, Smax).astype(int)
 
 # record each episode's cumulative reward to measure online performance
 Gzm = []
@@ -94,11 +123,13 @@ def evaluate_policy_milestone_test(Q, num_reps=num_test_reps, seed_mult=1):
     test_data = np.zeros((num_reps))
     # run num_test_reps replication per test
     for rep in range(num_reps):
-        terminated = False        # initialize episode conditions
+        # initialize episode conditions
+        terminated = False
         truncated = False
-        Gtest = 0                 # Initialize episode reward
-        state = env.reset(seed=seed_mult*1000+rep)[0]
+        # Initialize episode reward
+        Gtest = 0
         # Initialize the system by resetting the environment, obtain state var
+        state = env.reset(seed=seed_mult*1000+rep)[0]
         while not (terminated or truncated):
             # select action with highest q-value
             action = np.argmax(Q[tuple(phi(state))])
@@ -119,7 +150,10 @@ for z in range(Z):
     Q = qinit*np.ones(SAsize) * (qrange[1] - qrange[0]) + qrange[0]
     # initialize state-action counter
     C = np.zeros(SAsize)
+
     print(f"\n{nm1+1} - step SARSA(alpha_a = {alpha_a:<3.2f}, alpha_b={alpha_b:<3.2f}, eps_a={eps_a:<3.2f}, eps_b={eps_b:<3.2f}, |S|/d {Sintervals}, qinit={qinit}, rep {z}...")
+    
+    
     for m in range(M):
         # initialize episode complete flag (when system enters terminal state)
         terminated = False
@@ -139,7 +173,8 @@ for z in range(Z):
         # Select action based on epsilon-greedy exploration mechanism
         if np.random.rand() > epsilon(cs):
             # Act greedy by exploiting current knowledge
-            action = np.argmax(Q[tuple(state)])  # using Q
+            # using Q
+            action = np.argmax(Q[tuple(state)])  
         else:
             # Act randomly with probability epsilon to explore
             action = np.random.randint(0, num_actions)
@@ -165,12 +200,14 @@ for z in range(Z):
             # Select action based on epsilon-greedy exploration mechanism
             if np.random.rand() > epsilon(cs):
                 # Act greedy by exploiting current knowledge
+
                 next_action = np.argmax(Q[tuple(next_state)])
             else:
                 # Act randomly with probability epsilon to explore
                 next_action = np.random.randint(0, num_actions)
             # Append next action to action array
             action_queue.append(next_action)
+
         # SARSA main loop (first nml transitions until end of episode)
         while not (terminated or truncated):
             # Apply action and observe system information
@@ -195,16 +232,21 @@ for z in range(Z):
                 next_action = np.random.randint(0, num_actions)
             # append next action to action array
             action_queue.append(next_action)
+
             # temporal difference learning mechanism
-            qhat = np.dot(reward_queue, gamma**np.array(range(len(reward_queue)))) + \
+            qhat = np.dot(reward_queue, gamma**np.arange(len(reward_queue))) + \
                    (1-terminated) * gamma**len(reward_queue) * Q[tuple(next_state) + (next_action,)]
+            
             # update state-action counter (for earliest state-action in queue)
             state_action = tuple(state_queue[0]) + (action_queue[0],)
             C[state_action] += 1
+
             # determine number of times current state-action pair visited
             csa = C[state_action]
+
             # update Q
             Q[state_action] = (1-alpha(csa)) * Q[state_action] + alpha(csa) * qhat
+
             # update state, action, and reward arrays by removing oldest values
             state_queue.popleft()
             action_queue.popleft()
@@ -215,15 +257,19 @@ for z in range(Z):
             last_state_action = tuple(state_queue[-1]) + (action_queue[-1],)
             while len(reward_queue)>0:
                 # compute qhat
-                qhat = np.dot(reward_queue, gamma**np.array(range(len(reward_queue)))) + \
+                qhat = np.dot(reward_queue, gamma**np.arange(len(reward_queue))) + \
                        (1-terminated) * gamma**len(reward_queue) * Q[last_state_action]
+                
                 # update state-action counter
                 state_action = tuple(state_queue[0]) + (action_queue[0],)
                 C[state_action] += 1
+                
                 # determine number of times current state-action pair visited
                 csa = C[state_action]
+                
                 # update Q
                 Q[state_action] = (1-alpha(csa)) * Q[state_action] + alpha(csa) * qhat
+                
                 # update state, action, and reward arrays by removing oldest values
                 state_queue.popleft()
                 action_queue.popleft()
@@ -233,10 +279,11 @@ for z in range(Z):
             print(f"\nIn Episode: {m}, Cumulative reward: {Gm}")
             Gzm.append((z, Gm))
             
-            # test current policy (as represented by current Q) every test_freq episodes
-            if m % test_freq == 0:
-                mean, hw = evaluate_policy_milestone_test(Q, num_test_reps)
-                GzmTest.append((z,m, mean, hw))
+        # test current policy (as represented by current Q) every test_freq episodes
+        if m % test_freq == 0:
+            mean, hw = evaluate_policy_milestone_test(Q, num_test_reps)
+            GzmTest.append((z,m, mean, hw))
+
             # update best EETDR scores if necessary
             if update_best_scores(mean, hw, Q, best_scores):
                 print(f"Test... Episode: {m:>4}, EETDR CI: {mean:>6.1f} +/- {hw:4.1f}) *** New Top {num_best_scores} Reliable EETDR")
@@ -255,6 +302,7 @@ for z in range(Z):
 
 toc = time.perf_counter()
 print(f"\nExecuted {Z} algorithm reps in {toc - tic:0.4f} seconds.")
+
 print(f"\nIdentifying superlative policy via testing of policies from Top {num_best_scores} Reliable EETDRs...")
 
 # create string for ordinal number
@@ -283,7 +331,9 @@ maxETDRhw = hw_values[argmaxETDR]
 # Q-values representing superlative policy for algorithm design run
 Q = np.copy(best_scores[argmaxETDR]['Q'])
 
-# plot performance results
+"""
+plot performance results
+"""
 npGzmTest = np.array(GzmTest)
 size = np.shape(npGzmTest)
 TestEETDR = np.reshape(npGzmTest[:,2], (Z, int(size[0]/Z)))
@@ -297,6 +347,10 @@ avgTestEETDR = np.mean(TestEETDR, axis=0)
 avgTestSE = np.std(TestEETDR, axis=0)/np.sqrt(Z)
 avgTestHW = t.ppf(1-0.05/2, Z-1) * avgTestSE
 
+print(np.arange(0,M+1,test_freq).shape)
+print(np.linspace(0,M+1,Z).shape)
+#print(np.arange(0,M+1,(M+1)/TestEETDR.shape[0]).shape)
+print(TestEETDR.shape) # Z by 
 AULC = [metrics.auc(np.arange(0,M+1,test_freq), TestEETDR[z,:])/M for z in range(Z)]
 meanAULC = np.round(np.mean(AULC),1)
 hwAULC = t.ppf(1-0.05/2, Z-1) * np.std(AULC)/np.sqrt(Z)
@@ -308,13 +362,16 @@ plt.plot(np.arange(0,M+1,test_freq), avgTestEETDR-avgTestHW, color = 'blue', lin
 plt.xlabel('Episode')
 plt.ylabel('Mean Estimated Expected\nTotal Discounted Reward (EETDR)')
 plt.title(f'{nm1+1}-step SARSA Algorithm Test Performance, {Z} reps, {np.round((toc-tic)/(Z)/60,1)} min/rep\ngamma={gamma}, alpha_a={alpha_a}, alpha_b={alpha_b}, eps_a={eps_a}, eps_b={eps_b}, |S|/d={Sintervals}, qinit={qinit} \nMean Max EETDR: {meanMaxTestEETDR:>6.1f} +/- {maxTestHW:4.1f}, Mean Time-Avg EETDR={meanAULC} +/- {hwAULC:4.1f}\nSuperlative Policy EETDR: {maxETDR:>6.1f} +/- {maxETDRhw:4.1f}', fontsize=9)
+
 plt.legend(loc="lower right", fontsize=7)
 plt.xlim([-0.05*M, 1.05*M])
 plt.ylim([-205, -95])
 plt.grid(which='both')
 plt.show()
 
-# display superlative policy (sans exploration)
+"""
+display superlative policy (sans exploration)
+"""
 def displayBestPolicy():
     env = gym.make('MountainCar-v0', render_mode='human')
     # number of runs to show
